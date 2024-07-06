67409b64b22d1ebf58979e51eb7c4ec520f502c1
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-07-06 21:21:30 +0700

    feat: setup general components

---
+++ src/app/components/general/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface FooterProops {
+  title: string;
+  href: string;
+  logo?: string;
+}
+
+function Footer() {
+  return <></>;
+}
+
+export default Footer;
---
+++ src/app/components/general/Text.tsx
@@ -0,0 +1,11 @@
+import clsx from "clsx";
+import React from "react";
+
+interface textProops {
+  children?: string;
+  className?: string;
+}
+
+export function Text() {
+  return <div className={clsx()}></div>;
+}
