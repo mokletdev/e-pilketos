9af6eea03ca6a5597f703e94e4c6b3f6fe2f4f1d
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-09-25 20:45:53 +0700

    Refactor CSS classes in confirmation and vote pages

--- src/app/(users)/confirmation/page.tsx
+++ src/app/(users)/confirmation/page.tsx
@@ -5,7 +5,7 @@ import SectionsGap from "@/app/components/general/SectionsGap";
 export default function Confirmation() {
   return (
     <>
-      <main className="bg-red-light-6 w-full h-full min-h-screen">
+      <main className="bg-red-light-6 w-full h-full min-h-screen pt-16 lg:pt-0">
         <SectionsGap>
           <div className="mx-[22px] lg:mx-[44px] 2xl:mx-[88px]">
             <div className="flex justify-center items-center">
